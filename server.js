b6db083151b4eb398a585f530b4c977be61a6803
Cassper2022 <118291338+Cassper2022@users.noreply.github.com>
2022-12-13 20:15:37 +0200

    Env Addition and few worekaround

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// הפעלת פונקצייה מתוך ספריית אי אן וי שטוענת את ההגדרות מקובץ דואט אי אן וי
+require('dotenv').config();
+function sendEmail(u,p)
+{
+    console.log("sent Email to " + u);
+    // קרא לגוגל והתחבר באמצעות השם והסיסמא
+}
+SendEmail(process.env.USER_EMAIL, process.env.EMAIL_PASS);
 const http=require('http');
 const app=require('./app');
 const port=3000;
